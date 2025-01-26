7b3af19311c5df5b26b2cd716f352dfb0178774c
shakil <shakil.sarkar1932@gmail.com>
2025-01-26 17:56:33 +0600

    all is ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      'xs': '400px',
+      // => @media (min-width: 400px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 1024px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
